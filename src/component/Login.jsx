622615b1ca7b8e67aeb792c2f7402db587c6d9a7
sanjeev808 <mauryasanjeev808@gmail.com>
2022-04-28 18:13:19 +0530

    initial commit

---
+++ src/component/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default function Login() {
+  return (
+    <div className='container'>
+        <div className='row'>
+            <div className='col'>
+   </div> Login</div>
+            </div>
+         
+  )
+}
